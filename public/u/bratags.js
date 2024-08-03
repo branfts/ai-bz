c3705922093279790134ca23ec16b724342a2f12
branfts <support@june07.com>
2024-08-03 15:59:27 -0700

    Updated by A10x Service

---
+++ public/u/bratags.js
@@ -0,0 +1,11 @@
+
+const links = [
+    { url: 'mailto:support@bratags.com' },
+{ url: 'https://www.tiktok.com/@bratags' },
+{ url: 'https://toptop.bratags.com' }
+]
+const rules = [
+    
+]
+
+export { links, rules }
